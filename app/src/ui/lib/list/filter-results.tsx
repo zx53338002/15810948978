5d54237df8e2ed5636803cb3118a07e3bf6cbb5f
Brendan Forster <github@brendanforster.com>
2018-04-20 17:18:03 -0500

    writing some docs

--- app/src/ui/lib/list/filter-results.tsx
+++ app/src/ui/lib/list/filter-results.tsx
@@ -3,24 +3,38 @@ import * as React from 'react'
 import { List, findNextSelectableRow, SelectionSource } from '../../lib/list'
 
 interface IFilterResultProps {
+  /** The number of rows in the current filtered list */
   readonly rowCount: number
+
+  /** The set of selected rows in the current filtered list */
   readonly selectedRows: ReadonlyArray<number>
+
+  /** Callback to fire to render an element in the lists */
   readonly renderRow: (index: number) => JSX.Element | null
+
+  /** Optional callback to render something when no items are available */
   readonly renderNoItems?: () => JSX.Element | null
 
-  /** The height of the rows. */
+  /** The height of each row in the list. */
   readonly rowHeight: number
 
   /** Any props which should cause a re-render if they change. */
   readonly invalidationProps: any
 
+  /** Callback to fire when the selected row changes */
   readonly onSelectedRowChanged?: (
     index: number,
     source: SelectionSource
   ) => void
 
+  /**
+   * Function to validate if the given row can be selected by the user
+   */
   readonly canSelectRow: (index: number) => boolean
 
+  /**
+   * Callback to the parent component that it should focus on the filter text box
+   */
   readonly onFocusTextBox: (event: React.KeyboardEvent<any>) => void
 }
 
