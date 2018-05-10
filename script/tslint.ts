946661939b3b45d235d236f767b395f5f6ead817
Brendan Forster <github@brendanforster.com>
2018-05-10 11:03:28 +1000

    add new script to emulate inline package.json script

---
+++ script/tslint.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env ts-node
+
+import * as Path from 'path'
+import { spawnSync, SpawnSyncOptions } from 'child_process'
+
+// ensure commands are executed from the root of the repository
+const repositoryRoot = Path.dirname(__dirname)
+
+const options: SpawnSyncOptions = {
+  cwd: repositoryRoot,
+  // ensure stdout/stderr is propagated to the parent process so the test results
+  // are displayed to the user
+  stdio: 'inherit',
+}
+
+const tslint = process.platform === 'win32' ? 'tslint.cmd' : 'tslint'
+
+const tslintPath = Path.join(repositoryRoot, 'node_modules', '.bin', tslint)
+
+const tslintArgs = [
+  './{script,tslint-rules}/*.ts',
+  './app/{src,typings,test}/**/*.{ts,tsx}',
+]
+
+const { status } = spawnSync(tslintPath, tslintArgs, options)
+process.exitCode = status
